b60b0d5c904b5560ed64ac40dd6bbaee810095b7
Sella Rafaeli <sella.rafaeli@gmail.com>
2016-11-12 21:00:14 +0200

    clean results from mongo

--- server/mongoDB.js
+++ server/mongoDB.js
@@ -1,21 +1,17 @@
 require('mongodb').MongoClient.connect('mongodb://127.0.0.1:27017/foo', (err, db) => {
   Mongo = db;
   Users = db.collection('users');
+
   var cp = Users.__proto__; //collectionPrototype
 
-  // cp.add = function(doc) {
-  //   doc['created_at'] = new Date
-  //   doc['_id'] = niceId();
-  //   return this.insert(doc);  
-  // }
-  cp.add = function(fields) {
+  cp.add = function(fields) { //also upserts?
     fields['created_at'] = new Date;
     return this.findAndModify(
       {_id: niceID()}, //criteria
       [], //sorting?
       {$set: fields},
       {new: true, upsert: true} //opts: return the new (modified) doc
-    );
+    ).then((doc) => doc.value);
   }
 
   cp.updateID = function(_id, fields) {
@@ -24,6 +20,6 @@ require('mongodb').MongoClient.connect('mongodb://127.0.0.1:27017/foo', (err, db
       [], //sorting?
       {$set: fields},
       {new: true} //opts: return the new (modified) doc
-    );
+    ).then((doc) => doc.value);
   }
 });
--- server/routes/users.js
+++ server/routes/users.js
@@ -2,8 +2,12 @@ app.get('/users/all', (req,res) => {
   Users.find().toArray().then(sendThis(res));
 });
 
+app.get('/users/', (req,res) => {
+  res.redirect('/users/all');
+})
+
 app.get('/users/addOne', (req,res) => {
-  Users.add(req.query).then((doc) => res.send(doc.value));
+  Users.add(req.query).then(sendThis(res));
 });
 
 app.get('/users/deleteAll', (req,res) => {
@@ -12,5 +16,5 @@ app.get('/users/deleteAll', (req,res) => {
 
 app.get('/users/updateOne', (req, res) => {
   var qry = req.query;
-  Users.updateID(qry._id,qry).then((doc) => res.send(doc.value));
+  Users.updateID(qry._id,qry).then(sendIt(res));
 });
